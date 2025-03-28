8596e99c2912c3d41df3ccb42b063407b7159f4b
joão pedro silva <130461387+jprime617@users.noreply.github.com>
2025-03-27 21:34:12 -0300

    adicional js

--- ATV_JS.js
+++ ATV_JS.js
@@ -14,4 +14,26 @@ function vendas() {
     }
 }
 
-vendas()
+function categorias() {
+    categoria = Math.floor(Math.random() * 5) + 1;
+    preco = 0
+
+    if (categoria == 1) {
+        preco = 10
+    }else if (categoria == 2) {
+        preco = 18
+    }else if (categoria == 3) {
+        preco = 23
+    }else if (categoria == 4) {
+        preco = 26
+    }else if (categoria == 5) {
+        preco = 31
+    }else{
+        return console.log("Categoria invalida")
+    }
+
+    console.log(`O preço do produto é R$:${preco}`)
+}
+
+// vendas()
+categorias()
